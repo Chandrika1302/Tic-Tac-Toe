07b09905474d2beb7123715293e73f93257e864d
chandrika <gehini1302@gmail.com>
2022-04-19 20:47:12 +0530

    add winner logic

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import "../src/style.css";
 const gameMainDiv = document.querySelector("#game-grid");
 const gameGridItems = document.querySelectorAll(".items");
 const heading=document.querySelector('#haha');
+const winsDiv=document.querySelector('.win');
+
+const body = document.body;
+
 
 const gameBoard = {
   board: [],
@@ -20,6 +24,7 @@ const player2 = {
 let count = 0;
 
 function fun(e) {
+  
   const gridBox = e.target;
   if(heading.innerText=='player1'){ 
     if (gridBox.className == "items" && count % 2 == 0 && count < 10) {
@@ -27,8 +32,6 @@ function fun(e) {
       gridBox.innerText = player1.marker;
       gameBoard.board.push(gridBox.innerText);
       count += 1;
-      // console.log(count);
-      // console.log(gameBoard.board);
       heading.innerText='player2';
     }
   winner();
@@ -40,42 +43,42 @@ function fun(e) {
         gridBox.innerText = player2.marker;
         gameBoard.board.push(gridBox.innerText);
         count += 1;
-        // console.log(count);
-        // console.log(gameBoard.board);
       heading.innerText='player1';
       }
   }
   winner();
 
 }
+if(winsDiv.innerText=='x wins!!'){
+  body.removeEventListener('click',fun); 
+}
   } 
 const winner=function(){
     if(gameGridItems[0].innerText=='x' && gameGridItems[1].innerText=='x' && gameGridItems[2].innerText=='x'){
-        console.log('x wins!!');
+        winsDiv.innerText=('x wins!!');
     }
     else if(gameGridItems[0].innerText=='x' && gameGridItems[3].innerText=='x' && gameGridItems[6].innerText=='x'){
-        console.log('x wins!!');
+        winsDiv.innerText='x wins!!';
     }
     else if(gameGridItems[0].innerText=='x' && gameGridItems[4].innerText=='x' && gameGridItems[8].innerText=='x'){
-        console.log('x wins!!');
+        winsDiv.innerText='x wins!!';
     }
     else if(gameGridItems[3].innerText=='x' && gameGridItems[4].innerText=='x' && gameGridItems[5].innerText=='x'){
-        console.log('x wins!!');
+        winsDiv.innerText='x wins!!';
     }
     else if(gameGridItems[2].innerText=='x' && gameGridItems[5].innerText=='x' && gameGridItems[8].innerText=='x'){
-        console.log('x wins!!');
+        winsDiv.innerText='x wins!!';
     }
     else if(gameGridItems[1].innerText=='x' && gameGridItems[4].innerText=='x' && gameGridItems[7].innerText=='x'){
-        console.log('x wins!!');
+        winsDiv.innerText='x wins!!';
     }
     else if(gameGridItems[6].innerText=='x' && gameGridItems[7].innerText=='x' && gameGridItems[8].innerText=='x'){
-        console.log('x wins!!');
+        winsDiv.innerText='x wins!!';
     }
 }
-function init(){
-  const body = document.body;
-  body.addEventListener("click", fun);
 
+function init(){
+  body.addEventListener("click", fun);
 }
 init();
 
